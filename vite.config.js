105094d8f59ebb1f76f3b0082de53ae9d9c07df1
CareerMedia <careermedia@csun.edu>
2025-09-08 10:25:14 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // <-- keep this
+  base: './', // important for subpath deployments like user.github.io/repo
 })
