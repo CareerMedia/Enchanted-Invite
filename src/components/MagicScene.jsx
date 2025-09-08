227200752bed235ff06c080375eb7124b2155334
CareerMedia <careermedia@csun.edu>
2025-09-08 10:26:38 -0700

    Update MagicScene.jsx

--- src/components/MagicScene.jsx
+++ src/components/MagicScene.jsx
@@ -1,24 +1,22 @@
 import React from 'react'
-import { Environment, Stars } from '@react-three/drei'
+import { Stars } from '@react-three/drei'
 
 export default function MagicScene({ moonRef }) {
   return (
     <>
       <color attach="background" args={['#070813']} />
-      <fog attach="fog" args={['#0a0b1a', 10, 45]} />
-      {/* Ambient/Fill */}
-      <ambientLight intensity={0.35} color={'#d2d6ff'} />
-      {/* Moonlight */}
-      <directionalLight position={[-6, 10, 2]} intensity={0.6} color={'#c9d1ff'} castShadow />
-      {/* Silver moon for god rays */}
+      <fog attach="fog" args={['#0a0b1a', 18, 60]} />
+
+      <ambientLight intensity={0.7} color={'#dfe3ff'} />
+      <directionalLight position={[-4, 8, 6]} intensity={1.2} color={'#e7ebff'} castShadow />
+      <directionalLight position={[6, 4, -6]} intensity={0.5} color={'#99a4ff'} />
+
       <mesh ref={moonRef} position={[-10, 14, -12]}>
         <sphereGeometry args={[2.2, 48, 48]} />
-        <meshStandardMaterial emissive={'#dfe6ff'} emissiveIntensity={1.6} color={'#f2f5ff'} roughness={0.6} metalness={0} />
+        <meshStandardMaterial emissive={'#dfe6ff'} emissiveIntensity={1.2} color={'#f2f5ff'} roughness={0.6} />
       </mesh>
 
-      {/* Sky */}
-      <Stars radius={140} depth={90} count={8000} factor={4} saturation={0} fade speed={0.3} />
-      <Environment preset="night" />
+      <Stars radius={140} depth={90} count={5000} factor={4} saturation={0} fade speed={0.3} />
     </>
   )
 }
